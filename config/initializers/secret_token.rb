708fd56effe2dd9db73bcbb612632b93fb27ff6d
Tatsuya Ono <ononoma@gmail.com>
2012-07-28 17:08:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joyance::Application.config.secret_token = '80465967fedef49585879ce0e047cdc343d793ae7595dca388d733777c240c45d35c2661a6be77b80058a7f12c9c72a17d40fd18b2979c18250e538dc2cc211b'
