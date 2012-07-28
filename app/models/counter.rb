c2d494266bffa020dad0152ded618abc76e052a0
Tatsuya Ono <ononoma@gmail.com>
2012-07-28 17:21:02 +0100

    Get datasift user name and api key from env var

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,3 +1,11 @@
 class Counter < ActiveRecord::Base
   attr_accessible :count, :date, :gender, :hour, :minute, :sentiment, :stream
+
+  def user_id
+    ENV["DATASIFT_USERNAME"]
+  end
+
+  def api_key
+    ENV["DATASIFT_KEY"]
+  end
 end
