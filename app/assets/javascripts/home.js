2f8e55957ccd47b79945747881a9b0dc4150ba3c
Tak Tran <contact@tutaktran.com>
2012-07-29 03:37:04 +0100

    Another attempt at the pie graph, but with labels.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,32 +9,49 @@ App.config = {
 };
 
 function renderLoveHateGraph(data) {
+    var w = App.config.canvasWidth,
+    h = App.config.canvasHeight,
+    r = Math.min(w, h) / 2,
+    color = d3.scale.category20c();     //builtin range of colors
 
-  var w = App.config.canvasWidth,
-      h = App.config.canvasHeight,
-      r = Math.min(w, h) / 2,
-      color = d3.scale.category20(),
-      donut = d3.layout.pie()
-        .sort(null)
-        .value(function(d) {
-          return d.total; // Use the 'total' from the hash
-        }),
-      arc = d3.svg.arc().innerRadius(r - 100).outerRadius(r - 20);
+    var vis = d3.select("body")
+        .append("svg:svg")              //create the SVG element inside the <body>
+        .data([data])                   //associate our data with the document
+            .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+            .attr("height", h)
+        .append("svg:g")                //make a group to hold our pie chart
+            .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
 
-  var svg = d3.select("body").append("svg:svg")
-      .attr("width", w)
-      .attr("height", h)
-    .append("svg:g")
-      .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+        .outerRadius(r);
 
-  var arcs = svg.selectAll("path")
-      .data(donut(data))
-    .enter().append("svg:path")
-      .attr("fill", function(d, i) { return color(i); })
-      .attr("d", arc)
-      .each(function(d) { this._current = d; });
+    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+        .value(function(d) { return d.total; });    //we must tell it out to access the value of each element in our data array
 
-}
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+            .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+                .attr("class", "slice");    //allow us to style things in the slices (like text)
+
+        arcs.append("svg:path")
+                .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+                .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+
+        arcs.append("svg:text")                                     //add a label to each slice
+                .attr("transform", function(d) {                    //set the label's origin to the center of the arc
+                //we have to make sure to set these before calling arc.centroid
+                d.innerRadius = 0;
+                d.outerRadius = r;
+                return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+            })
+            .attr("text-anchor", "middle")                          //center the text on it's origin
+            .text(function(d, i) {
+              var overallTotal = _.reduce(data, function(memo, d) { return memo + d.total; }, 0)
+              var percentage = Math.round(data[i].total / overallTotal * 100);
+              return data[i].sentiment + " (" + percentage + "%)";
+            });
+  }
 
 $(function (data) {
   // Get json data
